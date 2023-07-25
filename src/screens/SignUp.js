1f5efdfd008f6e8155c0c6c8e523847a2abe9235
vivianlee03 <vivian@u.sogang.ac.kr>
2023-07-25 12:33:08 +0900

    Sign up with Backend -#11.12

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -11,6 +11,9 @@ import Input from "../components/auth/Input";
 import { styled } from "styled-components";
 import { HelmetProvider } from "react-helmet-async";
 import PageTitle from "../components/PageTitle";
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -36,7 +39,59 @@ const Info = styled.div`
   color: #737373;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 const SignUp = () => {
+  const history = useHistory();
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    history.push(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+
   return (
     <HelmetProvider>
       <AuthLayout>
@@ -48,18 +103,53 @@ const SignUp = () => {
               Sign up to see photos and videos from your friends.
             </Subtitle>
           </HeaderContainer>
-          <form>
+          <form onSubmit={handleSubmit(onSubmitValid)}>
             <Button type="submit" value="Log in with Facebook" />
             <Separator />
-            <Input type="text" placeholder="Mobile Number or Email" />
-            <Input type="text" placeholder="Full Name" />
-            <Input type="text" placeholder="Username" />
-            <Input type="password" placeholder="Password" />
+            <Input
+              {...register("firstName", {
+                required: "First name is required.",
+              })}
+              type="text"
+              placeholder="First Name"
+            />
+            <Input
+              {...register("lastName", {
+                required: "Last name is required.",
+              })}
+              type="text"
+              placeholder="Last Name"
+            />
+            <Input
+              {...register("username", {
+                required: "Username is required.",
+              })}
+              type="text"
+              placeholder="Username"
+            />
+            <Input
+              {...register("email", {
+                required: "Email is required.",
+              })}
+              type="text"
+              placeholder="Email"
+            />
+            <Input
+              {...register("password", {
+                required: "Password is required.",
+              })}
+              type="password"
+              placeholder="Password"
+            />
             <Info>
               People who use our service may have uploaded your contact
               information to Instagram. Learn more
             </Info>
-            <Button type="submit" value="Sign up" />
+            <Button
+              type="submit"
+              value={loading ? "Loading..." : "Sign Up"}
+              disabled={!isValid || loading}
+            />
           </form>
         </FormBox>
         <BottomBox
