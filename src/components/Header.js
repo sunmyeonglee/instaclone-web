f10a3219b1e87edb194aa372b612db863f70ccb7
vivianlee03 <vivian@u.sogang.ac.kr>
2023-07-25 13:55:10 +0900

    Header with backend (ing) -#12.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,9 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { isLoggedInVar } from "../apollo";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import routes from "../routes";
+import useUser from "../hooks/useUser";
 
 const SHeader = styled.header`
   width: 100%;
@@ -30,8 +33,17 @@ const Icon = styled.span`
   margin-left: 15px;
 `;
 
+const Button = styled.span`
+  background-color: ${(props) => props.theme.accent};
+  border-radius: 4px;
+  padding: 3px 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 function Header() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const loggedInUser = useUser();
   return (
     <SHeader>
       <Wrapper>
@@ -51,7 +63,11 @@ function Header() {
                 <FontAwesomeIcon icon={faUser} size="lg" />
               </Icon>
             </>
-          ) : null}
+          ) : (
+            <Link href={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
         </Column>
       </Wrapper>
     </SHeader>
