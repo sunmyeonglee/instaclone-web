645e1641ca908e323e06160889b5aa1a612783db
vivianlee03 <vivian@u.sogang.ac.kr>
2023-07-29 13:25:49 +0900

    parsing hashtags with React.Fragment -#12.14

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -2,12 +2,14 @@ import { styled } from "styled-components";
 import { FatText } from "../shared";
 import PropTypes from "prop-types";
 import sanitizeHtml from "sanitize-html";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React from "react";
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 4px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.accent};
     cursor: pointer;
@@ -18,20 +20,22 @@ const CommentCaption = styled.span`
 `;
 
 const Comment = ({ author, payload }) => {
-  const cleanedPayload = sanitizeHtml(
-    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
-    {
-      allowedTags: ["mark"],
-    }
-  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleanedPayload,
-        }}
-      />
+      <CommentCaption>
+        {payload.split(" ").map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link key={index} to={`/hashtags/${word}`}>
+                {word}
+              </Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 };
