cbe4e471bfc1bf2bf5ab338926c0065b6cb0ead7
vivianlee03 <vivian@u.sogang.ac.kr>
2023-07-29 13:14:38 +0900

    parsing hashtags -#12.13

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,18 +1,37 @@
 import { styled } from "styled-components";
 import { FatText } from "../shared";
 import PropTypes from "prop-types";
+import sanitizeHtml from "sanitize-html";
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 4px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Comment = ({ author, payload }) => {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 };
