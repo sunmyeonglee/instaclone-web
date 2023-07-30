e8a983d2f680bd4d2e6469945ce75cd4f98e0695
vivianlee03 <vivian@u.sogang.ac.kr>
2023-07-30 11:29:00 +0900

    cache Modify -#12.15

--- src/components/feed/Photo.js
+++ src/components/feed/Photo.js
@@ -71,6 +71,7 @@ const Likes = styled(FatText)`
   margin-top: 12px;
   display: block;
 `;
+
 function Photo({
   id,
   user,
@@ -88,29 +89,21 @@ function Photo({
       },
     } = result;
     if (ok) {
-      const fragmentId = `Photo:${id}`;
-      const fragment = gql`
-        fragment AnyName on Photo {
-          isLiked
-          likes
-        }
-      `;
-      const result = cache.readFragment({
-        id: fragmentId,
-        fragment,
-      });
-
-      if ("isLiked" in result && "likes" in result) {
-        const { isLiked: cacheIsLiked, likes: cacheLikes } = result;
-        cache.writeFragment({
-          id: fragmentId,
-          fragment: fragment,
-          data: {
-            isLiked: !cacheIsLiked,
-            likes: cacheIsLiked ? cacheLikes - 1 : cacheLikes + 1,
+      const photoId = `Photo:${id}`;
+      cache.modify({
+        id: photoId,
+        fields: {
+          isLiked(prev) {
+            return !prev;
           },
-        });
-      }
+          likes(prev) {
+            if (isLiked) {
+              return prev - 1;
+            }
+            return prev + 1;
+          },
+        },
+      });
     }
   };
   const [toggleLikeMutation, { loading }] = useMutation(TOGGLE_LIKE_MUTATION, {
